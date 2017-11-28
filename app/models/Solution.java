fe7f633c44df427bc0dee05bcfef939c3b832a0a
David Allysson <davidmoreirainformatica@gmail.com>
2017-11-28 13:00:20 -0300

    corrigindo importação 2

--- app/models/Solution.java
+++ app/models/Solution.java
@@ -35,46 +35,48 @@ import play.data.validation.Constraints.Required;
 @SuppressWarnings("serial")
 @Entity
 public class Solution extends Model {
-	
+
 	@Id
 	@Constraints.Min(10)
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	public Long id;
-	
+
 	// wird das Quiz zum ersten mal belegt?
 	public boolean firstTime;
-	
+
 	public boolean firstQuestionShow;
 	// hat der Spieler das Quiz abgeschlossen?
 	public boolean done;
 	// die Zeit, die der Spieler (bisher) für das Quiz benötigt hat
 	public Long quizAnswerTime;
-	
+
 	public Long bestTime;
 	// die erzielten Punkte
 	public int points;
 	// falls das Quiz unterbrochen wird => Long (ID der Frage im Quiz)
 	public int currentQuestion;
-	
+
 	public Date start;
-	
+
 	public Integer codigoHabilitacaoNerd;
-	
+
 	@ManyToOne
+	@JsonBackReference
 	public Usuario quizParticipant;
-	
+
 	@ManyToOne
+	@JsonBackReference
 	public Quiz quiz;
-	
+
 	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	public Set<Questao> questions;
-	
+
 	public boolean paused;
-	
+
 	public int answeredCorreclty;
-	
+
 	public static Finder<Long, Solution> find = new Finder<Long,Solution>(Solution.class);
-	
+
 	public static Solution find(Usuario u, Quiz q) {
 		return find.where().eq("quiz", q).eq("quizParticipant", u).findUnique();
 	}
@@ -90,56 +92,56 @@ public class Solution extends Model {
 		firstQuestionShow = true;
 		startSolution();
 	}
-	
+
 	public Quiz getQuiz() {
 		return quiz;
 	}
-	
+
 	public Usuario getQuizParticipant() {
 		return quizParticipant;
 	}
-	
+
 	public int getPoints() {
 		return points;
 	}
-	
+
 	public Long getQuizAnswerTime() {
 		return quizAnswerTime;
 	}
-	
+
 	public void incCurrentQuestion() {
 		currentQuestion++;
 	}
-	
+
 	public void decCurrentQuestion() {
 		currentQuestion--;
 	}
-	
+
 	public boolean isDone() {
 		return done;
 	}
-	
+
 	public void setFirstTime(boolean val) {
 		firstTime = val;
 	}
-	
+
 	public void setDone(boolean val) {
 		done = val;
 	}
-	
+
 	public boolean isPaused() {
 		return paused;
 	}
-	
-	
+
+
 	public int getCurrentQuestion() {
 		return currentQuestion;
 	}
-	
+
 	public Long getBestTime() {
 		return bestTime;
 	}
-	
+
 	public int getAnsweredCorrectly() {
 		return answeredCorreclty;
 	}
@@ -150,7 +152,7 @@ public class Solution extends Model {
 		currentQuestion = 0;
 		answeredCorreclty = 0;
 	}
-	
+
 	public void resume() {
 		paused = false;
 		start = new Date();
@@ -160,7 +162,7 @@ public class Solution extends Model {
 		Date date = new Date();
 		quizAnswerTime += date.getTime() - start.getTime();
 	}
-	
+
 	public void finish() {
 		done = true;
 		firstTime = false;
@@ -174,7 +176,7 @@ public class Solution extends Model {
 			bestTime = quizAnswerTime;
 		System.out.println("\n\n\n BestTime: " + bestTime + " answerTime: " + quizAnswerTime);
 	}
-	
+
 	public Questao getQuestion(Long idSolutions) {
 		Solution sol = Solution.find.byId(idSolutions);
 		if(currentQuestion < quiz.getTotalRestante(sol)){
@@ -189,21 +191,21 @@ public class Solution extends Model {
 		 }
 		return null;
 	}
-	
+
 	public Questao getQuestion(Long idQuestion, Solution solution) {
 		return quiz.findQuestion(solution, idQuestion);
 	}
-	
-	public Questao getQuestionAletorio(Long idSolutions) { 
+
+	public Questao getQuestionAletorio(Long idSolutions) {
 		Solution sol = Solution.find.byId(idSolutions);
 		int valor = quiz.getTotalRestante(sol);
 		valor--;
 		if(valor < 0){
 			return null;
 		}
-		return quiz.getQuestionsAleatorio(sol).get(valor); 
+		return quiz.getQuestionsAleatorio(sol).get(valor);
 	}
-	
+
 	public int evaluateAnswer(String answer, Long idSolution, Long idQuestion) {
 		int result = -1; //Antwort nicht gueltig
 		Solution solution = Solution.find.byId(idSolution);
@@ -235,7 +237,7 @@ public class Solution extends Model {
 						pontosResposta = (copy.pontos * 3);
 						points =points+pontosResposta;
 					}
-					
+
 					PontuacaoEvasao pontuacao = new PontuacaoEvasao();
 					pontuacao.dataCadastro = new Date();
 					pontuacao.pergunta = q;
@@ -244,17 +246,17 @@ public class Solution extends Model {
 					pontuacao.usuario = InformacoesUsuarioHelper.getUsuarioLogado();
 					pontuacao.pontuacaoObtida = pontosResposta;
 					pontuacao.save();
-					
+
 					//TODO: Fazer esquema do calculo
 					/*
 					 * Obter a opção marcada pelo aluno no campo: ordem
 					 * --> A primeira opção marcada terá peso 4, a segunda opção marcada terá peso 3.
 //					 * Ex: 1° opção marcada = ordem * 4
-					 *     2° opção marcada = ordem * 3 
-					 * 
-					 * -> Quando marcar as duas questões de mesmo eixo ou pular é que  
+					 *     2° opção marcada = ordem * 3
+					 *
+					 * -> Quando marcar as duas questões de mesmo eixo ou pular é que
 					 */
-					
+
 					result = 1; //Antwort richtig und mit Punktzahl bewertet
 				} else
 					result = 2; //Antwort richtig ohne Punkte
@@ -269,9 +271,6 @@ public class Solution extends Model {
 		q.update();
 		return result;
 	}
-	
-		
-}
-	
-	
 
+
+}
